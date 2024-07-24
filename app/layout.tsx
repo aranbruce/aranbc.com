05c5f7f9f1914f4c7370bc0eb23128d9e3524cf0
Aran Bruce-Caddick <aranbruce@gmail.com>
2024-07-24 23:00:00 +0100

    added link to current employment

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-gray-100">
       <body className={urbanist.className}>
         <Header />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,57 @@ export default function Home() {
           <h1 className="text-primary text-4xl font-bold">👋 Hi I'm Aran</h1>
           <p className="text-secondary text-lg">
             I'm a product builder 🛠️, code enthusiast 🧑‍💻 and data nerd 🤓 who's
-            deeply AI-curious. I currently work at Crezco, building products
-            that make money easy to grow the global economy
+            deeply AI-curious. I currently work at{" "}
+            <span>
+              <Link
+                href="https://www.crezco.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="border-primary/10 bg-primary/5 hover:bg-primary/10 rounded-md border px-1 pb-0.5 transition"
+              >
+                <svg
+                  width="12"
+                  height="12"
+                  className="mr-1 inline-flex"
+                  viewBox="0 0 26 26"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clipPath="url(#clip0_482_522)">
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M25.9999 0V25.9999H20.7999V5.19999H0V0H25.9999Z"
+                      fill="#2A6355"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M20.7999 5.19922V25.9992H15.6V10.3992H0V5.19922H20.7999Z"
+                      fill="#9BCABE"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M15.6 10.3999V25.9999H10.4V15.5999H0V10.3999H15.6Z"
+                      fill="#F2EBE4"
+                    />
+                    <path
+                      d="M10.4 15.6001H0V26.0001H10.4V15.6001Z"
+                      fill="#F38173"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_482_522">
+                      <rect width="26" height="26" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                <span className="text-sm font-semibold">Crezco</span>
+              </Link>
+            </span>
+            , building products that make money easy to grow 🌱 the global
+            economy
           </p>
         </div>
         <Button
